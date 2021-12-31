970eec4ad486aab7b6936eadf9d3f0507bc316af
Yousof Wakili <ywakili18@gmail.com>
2021-12-31 12:21:09 -0800

    Contact form completed, responsive, form sends email

--- components/form/Form.js
+++ components/form/Form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { IoMdSend } from 'react-icons/io'
 const Form = () => {
   return (
     <form
@@ -8,37 +8,56 @@ const Form = () => {
       className="text-black  mx-auto"
     >
       <div className="border border-white rounded-lg flex flex-col text-center p-2 text-xs ">
+        <label
+          htmlFor="name"
+          className="text-violet-400 text-left text-lg sm:text-xl md:text-2xl"
+        >
+          Name
+        </label>
         <input
           type="text"
           name="name"
           placeholder="Name"
           required
-          className="bg-slate-900 text-white p-2 rounded-lg "
+          className="bg-gray-300  placeholder-gray-400 text-black p-2 rounded-lg "
         />
+        <label
+          htmlFor="email"
+          className="text-violet-400 text-left text-lg sm:text-xl md:text-2xl mt-2"
+        >
+          Email
+        </label>
         <input
           type="email"
           name="email"
           placeholder="Email"
           required
-          className="bg-slate-900 text-white mt-2 p-2 rounded-lg "
+          className="bg-gray-300  placeholder-gray-400 text-black mt-2 p-2 rounded-lg mb-2"
         />
-        <input
+        <label
+          htmlFor="message"
+          className="text-violet-400 text-left text-lg sm:text-xl md:text-2xl"
+        >
+          Your Message
+        </label>
+        <textarea
           type="message"
           name="message"
           placeholder="Your Message"
           required
-          className="bg-slate-900 text-white mt-2 p-2 rounded-lg "
+          className="bg-gray-300  placeholder-gray-400 text-black mt-2 p-2 rounded-lg "
         />
         <input
           type="hidden"
           name="_next"
-          value="http://localhost:3000/thankyou"
+          value="http://youseffect.com/thankyou"
         ></input>
         <button
           type="submit"
-          className="border rounded-lg text-white transition-all duration-500 ease-out hover:bg-purple-400 bg-slate-800"
+          className="border p-2 rounded-lg mt-2 text-xl text-white transition-all duration-500 ease-out hover:bg-purple-400 bg-slate-800 flex justify-center"
         >
-          Send Message
+          <p>Send Message</p>
+          <IoMdSend className="mt-1 ml-2" />
         </button>
       </div>
     </form>
--- pages/contact.js
+++ pages/contact.js
@@ -3,8 +3,15 @@ import Form from '../components/form/form'
 const Contact = () => {
   return (
     <div id="contact" className="p-20  flex flex-col justify-evenly mt-48">
-      <div className="text-center text-5xl">
-        <span className="text-violet-400">contact me.</span>
+      <div className="text-center">
+        <p className="text-violet-400 text-2xl sm:text-3xl md:text-5xl">
+          say hello.
+        </p>
+        <p className="text-lg sm:text-2xl md:text-3xl mt-5">
+          {' '}
+          Let me help build your new{' '}
+          <span className="text-violet-400">idea.</span>
+        </p>
       </div>
       <div className="mt-10">
         <Form />
