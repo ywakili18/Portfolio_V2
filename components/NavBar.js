4cf26d3872020777ff025c63130a78550c2ee957
Yousof Wakili <ywakili18@gmail.com>
2021-12-31 16:45:27 -0800

    focus ring added for navs

--- components/NavBar.js
+++ components/NavBar.js
@@ -13,26 +13,30 @@ const NavBar = () => {
       transition={{ delay: 1.4 }}
     >
       <nav
-        className="flex bg-zinc-800 text-slate-100 text-xs sm:text-xl justify-evenly font-Comfortaa
+        className="flex bg-zinc-800 text-slate-100 text-xs sm:text-xl justify-evenly font-Comfortaa 
       tracking-wide p-4 h-20 fixed top-0 inset-x-0 z-20 rounded-b-lg "
       >
         <Link href="/#">
-          <a className=" hover:text-blue-900 hover:bg-violet-400  rounded-lg  px-3  md:mt-0">
+          <a
+            className=" hover:text-blue-900 hover:bg-violet-400  rounded-lg  px-3  md:mt-0
+          active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300
+          "
+          >
             <p className="mt-3">home.</p>
           </a>
         </Link>
         <Link href="#projects">
-          <a className="hover:text-blue-900 hover:bg-violet-400  rounded-lg px-3 ">
+          <a className="hover:text-blue-900 hover:bg-violet-400  rounded-lg px-3 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300">
             <p className="mt-3">projects.</p>
           </a>
         </Link>
         <Link href="#contact">
-          <a className="hover:text-blue-900 hover:bg-violet-400  rounded-lg px-3 ">
+          <a className="hover:text-blue-900 hover:bg-violet-400  rounded-lg px-3 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300">
             <p className="mt-3">contact.</p>
           </a>
         </Link>
         <Link href="#about">
-          <a className="hover:text-blue-900 hover:bg-violet-400  rounded-lg px-3 ">
+          <a className="hover:text-blue-900 hover:bg-violet-400  rounded-lg px-3 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 ">
             <p className="mt-3">about.</p>
           </a>
         </Link>
