293d2e10c97297238bdd096ecc3998334f4c5098
Yousof Wakili <ywakili18@gmail.com>
2022-05-26 02:03:32 -0700

    meta data

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,10 @@ export default function Home() {
         />
         <meta name="author" content="Yousof wAKILI" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:image"
+          content="https://cdn-icons-png.flaticon.com/512/1157/1157109.png"
+        />
         <link
           rel="icon"
           href="https://cdn-icons-png.flaticon.com/512/1157/1157109.png"
